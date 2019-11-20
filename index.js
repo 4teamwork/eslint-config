c2cac55f13dd37a85ad6e359e540bbad1cfb7d57
Kevin Bieri <bierike@gmail.com>
2019-11-20 16:24:55 +0100

    Disable space before function paren rule
    This rule needs to be disabled to work together with prettier.

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 module.exports = {
-  plugins: [
-    'nuxt',
-    'cypress',
-    'prettier',
-  ],
+  plugins: ['nuxt', 'cypress', 'prettier'],
   extends: [
     // https://github.com/nuxt/eslint-config/blob/master/packages/eslint-config/index.js
     '@nuxtjs',
@@ -16,27 +12,34 @@ module.exports = {
     // https://github.com/nuxt/eslint-plugin-nuxt/blob/master/lib/configs/recommended.js
     'plugin:nuxt/recommended',
     // https://github.com/cypress-io/eslint-plugin-cypress/blob/master/lib/config/recommended.js
-    'plugin:cypress/recommended',
+    'plugin:cypress/recommended'
   ],
   rules: {
     // require trailing commas in multiline object literals
-    'comma-dangle': ['error', {
-      arrays: 'always-multiline',
-      objects: 'always-multiline',
-      imports: 'always-multiline',
-      exports: 'always-multiline',
-      functions: 'only-multiline',
-    }],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'only-multiline'
+      }
+    ],
     // no space before function parentheses
-    'space-before-function-paren': ['error', {
-      anonymous: 'always',
-      named: 'never',
-      asyncArrow: 'always'
-    }],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ],
     // disallow param reassign
     'no-param-reassign': ['error'],
     // allow console and debugger when developing
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
+    'space-before-function-paren': 'off'
+  }
 }
